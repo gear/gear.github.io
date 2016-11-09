4fa0410324c0e027cfa1422c3070381a4b81fe3a
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-11-09 19:23:36 +0900

    append timeline script

--- js/main.js
+++ js/main.js
@@ -136,3 +136,25 @@ var main = {
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
+
+jQuery(document).ready(function($) {
+  var timelineBlocks = $('.cd-timeline-block'), offset=0.8;
+  hideBlocks(timelineBlocks, offset);
+  $(window).on('scroll', function() {
+    (!window.requestAnimationFrame)
+      ? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+      : window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset);});
+  });
+
+  function hideBlocks(blocks, offset) {
+    blocks.each(function() {
+      ( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+    });
+  }
+
+  function showBlocks(blocks, offset) {
+    blocks.each(function() {
+      ( $(this).offset().top <= $(window).scrollTop() + $(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+    });
+  }
+});

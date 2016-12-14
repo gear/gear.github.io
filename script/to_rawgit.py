f6e2aacc78ebb3d3e97a6f13b1ddb89695e44846
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-12-14 20:17:40 +0900

    script convert to rawgit link

--- script/to_rawgit.py
+++ script/to_rawgit.py
@@ -1,16 +1,25 @@
-!#/usr/bin/python3
+#!/root/anaconda3/bin/python
 
 # Python 3.5.2
 # utf-8
 # Convert github link to raw link
 
 from sys import argv
+from urllib.parse import urlparse
 
 def main():
     """
     Main function to convert and print new link to console.
     """
-    assert len(argv) == 1, 'This function receives at most one string input!'
+    assert len(argv) == 2, "Usage: to_rawgit 'https://github.com/...'"
+    git_link = argv[1]
+    print('Link to convert:' + git_link)
+    parsed = urlparse(git_link)
+    content = parsed.path.split('/')
+    content.remove('blob')
+    new_path = '/'.join(content)
+    new_link = parsed._replace(netloc='cdn.rawgit.com', path=new_path)
+    print(new_link.geturl()) 
 
 if __name__ == '__main__':
     main()

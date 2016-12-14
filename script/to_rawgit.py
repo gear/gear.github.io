b7d03c5dfe74f00c2d6be476e4acab01e33957fb
root <root@localhost.localdomain>
2016-12-14 19:54:33 +0900

    add story

---
+++ script/to_rawgit.py
@@ -0,0 +1,16 @@
+!#/usr/bin/python3
+
+# Python 3.5.2
+# utf-8
+# Convert github link to raw link
+
+from sys import argv
+
+def main():
+    """
+    Main function to convert and print new link to console.
+    """
+    assert len(argv) == 1, 'This function receives at most one string input!'
+
+if __name__ == '__main__':
+    main()
